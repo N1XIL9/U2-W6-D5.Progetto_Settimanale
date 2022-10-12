59e4fe20d8af87998476bc044b80418adaed3751
Nicola Lerra <github.nicolalerra@gmail.com>
2022-10-12 18:19:48 +0200

    animazioni

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,3 +39,21 @@ toggle.addEventListener("click", function () {
   toggle.classList.toggle("bi-toggle-on");
   document.body.classList.toggle("darkMode");
 });
+
+// ANIMAZIONE FADE SITO
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
